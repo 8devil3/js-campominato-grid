5c6c8e8ac536d8cced7e5241e794ee261cf611f4
8devil3 <8dev.il3@gmail.com>
2022-03-02 14:43:10 +0100

    generazione griglie

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
-let divContainer = document.querySelector('.container');
+const divContainer = document.querySelector('.container');
+const selectLevelInput = document.querySelector('#level');
 let divNumberBox;
 
+const btnPlay = document.querySelector('#play');
 
-for (let x = 1; x <= 100; x++) {
-    divNumberBox = document.createElement('div');    
-    divNumberBox.innerHTML = x;
-    divContainer.append(divNumberBox);
+const lvEasy = 49;
+const lvMedium = 81;
+const lvHard = 100;
+
+btnPlay.addEventListener('click', gridGeneration);
+
+
+function gridGeneration() {
+
+    divContainer.innerHTML = ''; //reset container
+    
+    if (selectLevelInput.value == "1") {
+
+        for (let x = 1; x <= lvEasy; x++) {
+            divNumberBox = document.createElement('div');
+            divNumberBox.innerHTML = x;
+            divContainer.append(divNumberBox);
+            divContainer.classList.add('container-lvEasy');
+        }
+
+    } else if (selectLevelInput.value == "2") {
+
+        for (let x = 1; x <= lvMedium; x++) {
+            divNumberBox = document.createElement('div');
+            divNumberBox.innerHTML = x;
+            divContainer.append(divNumberBox);
+            divContainer.classList.add('container-lvMedium');
+        }
+
+    } else {
+
+        for (let x = 1; x <= lvHard; x++) {
+            divNumberBox = document.createElement('div');
+            divNumberBox.innerHTML = x;
+            divContainer.append(divNumberBox);
+            divContainer.classList.add('container-lvHard');
+        }
+    }
 }
+
+
